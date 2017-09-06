c20cc822ecea10364de63a0f38f4db36ebfb2df7
devuser <pythoner@icloud.com>
2017-09-06 09:35:42 +0800

    update readme.md 20170906

--- rundocker.sh
+++ rundocker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -it --name node-spider  -v $HOME/working/docker-spider:/usr/src/myapp -w /usr/src/myapp -p 43000:3000 -p 48080:8080 -p 40080:80 xiehuanang/node /bin/bash
+docker run --rm -it --name node-spider  -v $PWD:/usr/src/myapp -w /usr/src/myapp -p 43000:3000 -p 48080:8080 -p 40080:80 xiehuanang/node /bin/bash
